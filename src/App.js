f0a1b4a274509bcabbf8795eb107e7b886dddf86
Marylange <marylangess@gmail.com>
2020-08-19 19:18:57 -0300

    definindo rotas do app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import logo from './trivia.png';
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-        <Login />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={Login} />
+      {/* <Route exact path='/game' component={Game} />
+      <Route exact path='/feedback' component={Feedback} />
+      <Route exact path='/ranking' component={Ranking} /> */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            SUA VEZ
+          </p>
+          <Login />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
