e89ff8fff2ecf49a18e7ebd482ef172099a469d8
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-08-21 20:08:06 -0300

    app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Game from './components/Game';
 import Feedback from './pages/Feedback';
 import Login from './components/Login';
-<<<<<<< HEAD
-import Header from './components/Header';
-=======
 import Settings from './pages/Settings';
 import Ranking from './pages/Ranking';
->>>>>>> d45c9a8171bae231f40bb536f39e254c8ab0c2cb
 
 export default function App() {
   return (
@@ -18,13 +14,7 @@ export default function App() {
       <Route exact path='/ranking' component={Ranking} />
       <Route exact path='/game' component={Game} />
       <Route exact path='/' component={Login} />
-<<<<<<< HEAD
-      {/* <Route exact path='/game' component={Game} />
-      <Route exact path='/ranking' component={Ranking} /> */}
-      <Header />
-=======
       <Route exact path='/settings' component={Settings} />
->>>>>>> d45c9a8171bae231f40bb536f39e254c8ab0c2cb
     </BrowserRouter>
   );
 }
