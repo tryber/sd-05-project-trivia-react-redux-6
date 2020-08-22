0a209bc4a83b1b63f5ec9228d8fae0ac6881580f
Marylange <marylangess@gmail.com>
2020-08-22 16:51:43 -0300

    Adicionando campos da página de configurações

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -8,8 +8,31 @@ class Settings extends React.Component {
         <button>
           <Link to="/">Home</Link>
         </button>
-        <h2 data-testid="settings-title">configurações</h2>
-        <p>Em construção</p>
+        <h3 data-testid="settings-title">Configurações</h3>
+        <div>
+          <label htmlFor="category_field">Categoria</label>
+          <select name="category_field">
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+          <label htmlFor="difficulty_field">Dificuldade</label>
+          <select name="difficulty_field">
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+          <label htmlFor="type_field">Tipo</label>
+          <select name="type_field">
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+        </div>
+
       </div>
     );
   }
