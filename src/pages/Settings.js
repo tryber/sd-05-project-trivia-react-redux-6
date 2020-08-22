bb6cef623af4833b4cbe58265f5171e0f0a0b6ca
Marylange <marylangess@gmail.com>
2020-08-22 16:18:37 -0300

    adicionando caminho para home em configurações

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Settings extends React.Component {
   render() {
     return (
       <div>
-        <h1 data-testid="settings-title">configurações</h1>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <h2 data-testid="settings-title">configurações</h2>
+        <p>Em construção</p>
       </div>
     );
   }
